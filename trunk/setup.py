edd8e1bb7ca9a2c99e3feff9e42d7e791a4763c8
mario.balibrera <mario.balibrera@01cde5ee-0551-0410-84e8-6b550f1b3d53>
2008-07-14 23:22:14 +0000

    set_rmode_xml

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='dez',
-    version='0.2.9',
+    version='0.3.0',
     author='Michael Carter',
     author_email='mario.balibrera@gmail.com',
     download_url='http://code.google.com/p/dez/downloads/list',
