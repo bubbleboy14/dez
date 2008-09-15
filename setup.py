5ff97d9bb60f83b1f1c8c994dc7b7849174397e8
mario.balibrera <mario.balibrera@01cde5ee-0551-0410-84e8-6b550f1b3d53>
2008-09-15 07:53:16 +0000

    better exception handling for Connection class

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='dez',
-    version='0.3.7',
+    version='0.3.8',
     author='Michael Carter',
     author_email='mario.balibrera@gmail.com',
     download_url='http://code.google.com/p/dez/downloads/list',
