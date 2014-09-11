65c3a810630a21c983e164d8eca135cff02ff092
mario.balibrera@gmail.com <mario.balibrera@gmail.com@01cde5ee-0551-0410-84e8-6b550f1b3d53>
2014-09-11 07:01:33 +0000

    0.5.2.1: websocket connection utf-8 decodes incoming payload before json parsing it

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='dez',
-    version='0.5.2',
+    version='0.5.2.1',
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     download_url='http://code.google.com/p/dez/downloads/list',
