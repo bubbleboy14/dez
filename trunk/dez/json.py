8276ad2031e7c1322e661e930da2f816843eb155
mario.balibrera@gmail.com <mario.balibrera@gmail.com@01cde5ee-0551-0410-84e8-6b550f1b3d53>
2015-01-21 03:17:48 +0000

    0.5.3.2: dez.json tries native json before falling back to cjson, simplejson, or demjson

--- trunk/dez/json.py
+++ trunk/dez/json.py
@@ -3,18 +3,29 @@ Select a JSON library from any of several known libraries.
 """
 
 try:
-    import cjson
-    encode = cjson.encode
-    decode = cjson.decode
+    # first try regular json,
+    # but we need to import it weirdly
+    # because this module is also called json
+    import imp, sys
+    f, pathname, desc = imp.find_module("json", sys.path[1:])
+    js = imp.load_module("native_json", f, pathname, desc)
+    f and f.close()
+    encode = js.dumps
+    decode = js.loads
 except ImportError:
     try:
-        import simplejson
-        encode = simplejson.dumps
-        decode = simplejson.loads
+        import cjson
+        encode = cjson.encode
+        decode = cjson.decode
     except ImportError:
         try:
-            import demjson
-            encode = demjson.encode
-            decode = demjson.decode
+            import simplejson
+            encode = simplejson.dumps
+            decode = simplejson.loads
         except ImportError:
-            raise ImportError, "could not load one of: cjson, simplejson, demjson"
+            try:
+                import demjson
+                encode = demjson.encode
+                decode = demjson.decode
+            except ImportError:
+                raise ImportError, "could not load one of: json, cjson, simplejson, demjson"
