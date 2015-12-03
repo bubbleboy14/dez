677689db3b4fee94ec8a39bd41d12c0c940f5f20
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-02 17:38:22 -0800

    StaticHandler: simplified HTTP responses

--- dez/http/static.py
+++ dez/http/static.py
@@ -13,40 +13,42 @@ class StaticHandler(object):
             self.cache = NaiveCache()
             #print "static cache: NaiveCache"
 
+    def __respond(self, req, path=None, ctype=False, data=[], stream=False):
+        response = (stream and HTTPVariableResponse or HTTPResponse)(req)
+        response.headers['Server'] = self.server_name
+        if ctype:
+            response.headers['Content-type'] = self.cache.get_type(path)
+        if not path:
+            response.status = "404 Not Found"
+        for d in data:
+            response.write(d)
+        if stream:
+            self.__write_file(response, open(path), path)
+        else:
+            response.dispatch()
+
     def __call__(self, req, prefix, directory):
         path = os.path.join(directory, req.url[len(prefix):])
         if os.path.isdir(path):
             url = req.url
             if url.endswith('/'):
                 url = url[:-1]
-            response = HTTPResponse(req)
-            response.headers['Server'] = self.server_name
-            response.write('<b>%s</b><br><br>'%url)
-            response.write("<a href=%s>..</a><br>"%os.path.split(url)[0])
-            for child in os.listdir(path):
-                response.write("<a href=%s/%s>%s</a><br>"%(url,child,child))
-            return response.dispatch()
+            return self.__respond(req, data=[
+                '<b>%s</b><br><br>'%(url,),
+                "<a href=%s>..</a><br>"%(os.path.split(url)[0],)
+            ] + ["<a href=%s/%s>%s</a><br>"%(url,child,child) for child in os.listdir(path)])
         self.cache.get(req, path, self.__write, self.__stream, self.__404)
 
     def __write(self, req, path):
-        response = HTTPResponse(req)
-        response.headers['Server'] = self.server_name
-        response.headers['Content-type'] = self.cache.get_type(path)
-        response.write(self.cache.get_content(path))
-        response.dispatch()
+        self.__respond(req, path, True, [self.cache.get_content(path)])
 
     def __stream(self, req, path):
-        response = HTTPVariableResponse(req)
-        response.headers['Server'] = self.server_name
-        response.headers['Content-type'] = self.cache.get_type(path)
-        self.__write_file(response, open(path), path)
+        self.__respond(req, path, True, stream=True)
 
     def __404(self, req):
-        response = HTTPResponse(req)
-        response.headers['Server'] = self.server_name
-        response.status = '404 Not Found'
-        response.write("<b>404</b><br>Requested resource \"<i>%s</i>\" not found" % req.url)
-        response.dispatch()
+        self.__respond(req, data=[
+            "<b>404</b><br>Requested resource \"<i>%s</i>\" not found" % (req.url,)
+        ])
 
     def __write_file(self, response, openfile, path):
         data = openfile.read(io.BUFFER_SIZE)
