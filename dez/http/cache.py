d1c29d232625c127f55ef36c89f060d06159d75f
bubbleboy14 <mario.balibrera@gmail.com>
2015-12-07 17:47:07 -0800

    http cache: streaming init kwarg (streaming now determined by this bool instead of file cache status); moved INotifyCache.__update() to BasicCache, and call it on new path creation

--- dez/http/cache.py
+++ dez/http/cache.py
@@ -2,14 +2,20 @@ import mimetypes, os
 from dez.http.inotify import INotify
 
 class BasicCache(object):
-    def __init__(self):
+    def __init__(self, streaming=False):
         self.cache = {}
+        self.streaming = streaming
 
     def _mimetype(self, url):
         mimetype = mimetypes.guess_type(url)[0]
         if not mimetype:
             mimetype = "application/octet-stream"
         return mimetype
+
+    def __update(self, path):
+        f = open(path,'r')
+        self.cache[path]['content'] = f.read()
+        f.close()
 
     def get_type(self, path):
         return self.cache[path]['type']
@@ -20,12 +26,16 @@ class BasicCache(object):
     def add_content(self, path, data):
         self.cache[path]['content'] += data
 
+    def _return(self, req, path, write_back, stream_back):
+        (self.streaming and stream_back or write_back)(req, path)
+
     def get(self, req, path, write_back, stream_back, err_back):
         if self._is_current(path):
-            return write_back(req, path)
+            return self._return(req, path, write_back, stream_back)
         if os.path.isfile(path):
             self._new_path(path, req.url)
-            return stream_back(req, path)
+            self.__update(path)
+            return self._return(req, path, write_back, stream_back)
         err_back(req)
 
 class NaiveCache(BasicCache):
@@ -36,14 +46,10 @@ class NaiveCache(BasicCache):
         self.cache[path] = {'mtime':os.path.getmtime(path),'type':self._mimetype(url),'content':''}
 
 class INotifyCache(BasicCache):
-    def __init__(self):
+    def __init__(self, streaming=False):
         self.cache = {}
+        self.streaming = streaming
         self.inotify = INotify(self.__update)
-
-    def __update(self, path):
-        f = open(path,'r')
-        self.cache[path]['content'] = f.read()
-        f.close()
 
     def _is_current(self, path):
         return path in self.cache
