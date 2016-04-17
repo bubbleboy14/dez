fff8025b9220a4cf828f6bc09202b3c652e57650
bubbleboy14 <mario.balibrera@gmail.com>
2016-04-17 05:51:06 -0700

    removed json en/decoding from memcache module -- not necessary, slows things down

--- dez/memcache.py
+++ dez/memcache.py
@@ -1,17 +1,14 @@
-import json
-
 MC = None
 
 class Memcache(object):
 	def __init__(self):
 		self.cache = {}
 
-	def get(self, key, tojson=True):
-		val = self.cache.get(key)
-		return tojson and json.encode(val) or val
+	def get(self, key):
+		return self.cache.get(key)
 
-	def set(self, key, val, fromjson=True):
-		self.cache[key] = fromjson and json.decode(val) or val
+	def set(self, key, val):
+		self.cache[key] = val
 
 	def rm(self, key):
 		if key in self.cache:
