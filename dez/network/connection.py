ba5bb5e277e78c3f8ee8f75097f00d8cfc5e7375
bubbleboy14 <mario.balibrera@gmail.com>
2016-07-17 14:54:33 -0700

    Connection.__write_ready(): advance buffer by bytes sent (not buffer size)

--- dez/network/connection.py
+++ dez/network/connection.py
@@ -193,8 +193,7 @@ class Connection(object):
             self.__write_chunk = self.__write_queue.pop(0)
             self.__write_buffer.reset(self.__write_chunk.data)
         try:
-            bsent = len(self.__write_buffer)
-            self.sock.send(self.__write_buffer.get_value())
+            bsent = self.sock.send(self.__write_buffer.get_value())
             self.__write_buffer.move(bsent)
             return True
         except dez.io.socket.error, msg:
