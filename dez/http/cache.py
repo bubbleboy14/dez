3aabffff23beca618c31f09e722ffa920abb976c
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-23 13:11:56 -0700

    http cache stream mode: auto (stream any file larger than io.BUFFER_SIZE * 5 -- cache all others)

--- dez/http/cache.py
+++ dez/http/cache.py
@@ -1,14 +1,15 @@
 import mimetypes, os
 from dez.logging import default_get_logger
 from dez.http.inotify import INotify
+from dez import io
 
 class BasicCache(object):
     id = 0
-    def __init__(self, streaming=True, get_logger=default_get_logger):
+    def __init__(self, streaming="auto", get_logger=default_get_logger):
         BasicCache.id += 1
         self.id = BasicCache.id
         self.cache = {}
-        self.streaming = streaming
+        self.streaming = streaming # True|False|"auto"
         self.log = get_logger("%s(%s)"%(self.__class__.__name__, self.id))
         self.log.debug("__init__")
 
@@ -19,15 +20,22 @@ class BasicCache(object):
         return mimetype
 
     def __update(self, path):
-        self.log.debug("__update", path, self.streaming)
-        if self.streaming:
-            self.cache[path]['content'] = bool(os.stat(path).st_size)
-            self.log.debug("Content Present:", self.cache[path]['content'])
+        self.log.debug("__update", path)
+        if self._stream(path):
+            self.cache[path]['content'] = bool(self.cache[path]['size'])
         else:
             f = open(path,'r')
             self.cache[path]['content'] = f.read()
             f.close()
-            self.log.debug("Content Length:", len(self.cache[path]['content']))
+
+    def _stream(self, path):
+        p = self.cache[path]
+        p['size'] = os.stat(path).st_size
+        stream = self.streaming
+        if stream == "auto":
+            stream = p['size'] > io.BUFFER_SIZE * 5
+        self.log.debug("_stream", path, p['size'], stream)
+        return stream
 
     def get_type(self, path):
         return self.cache[path]['type']
@@ -39,20 +47,20 @@ class BasicCache(object):
         self.cache[path]['content'] += data
 
     def _empty(self, path):
-        return not self.cache[path]['content']
+        return not self.cache[path]['size']
 
     def _return(self, req, path, write_back, stream_back, err_back):
         if self._empty(path):
             err_back(req)
         else:
-            (self.streaming and stream_back or write_back)(req, path)
+            (self._stream(path) and stream_back or write_back)(req, path)
 
     def get(self, req, path, write_back, stream_back, err_back):
         if self._is_current(path):
             self.log.debug("get", path, "CURRENT!")
             self._return(req, path, write_back, stream_back, err_back)
         elif os.path.isfile(path):
-            self.log.debug("get", path, "CREATING FROM FILE!")
+            self.log.debug("get", path, "INITIALIZING FILE!")
             self._new_path(path, req.url)
             self.__update(path)
             self._return(req, path, write_back, stream_back, err_back)
@@ -68,7 +76,7 @@ class NaiveCache(BasicCache):
         self.cache[path] = {'mtime':os.path.getmtime(path),'type':self._mimetype(url),'content':''}
 
 class INotifyCache(BasicCache):
-    def __init__(self, streaming=True, get_logger=default_get_logger):
+    def __init__(self, streaming="auto", get_logger=default_get_logger):
         BasicCache.__init__(self, streaming, get_logger)
         self.inotify = INotify(self.__update)
 
