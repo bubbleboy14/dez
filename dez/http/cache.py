3c220261e9e0c6c76cb52520af819bc654c4d0c1
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-22 18:49:41 -0700

    http cache debug logs

--- dez/http/cache.py
+++ dez/http/cache.py
@@ -1,10 +1,16 @@
 import mimetypes, os
+from dez.logging import default_get_logger
 from dez.http.inotify import INotify
 
 class BasicCache(object):
-    def __init__(self, streaming=False):
+    id = 0
+    def __init__(self, streaming=False, get_logger=default_get_logger):
+        BasicCache.id += 1
+        self.id = BasicCache.id
         self.cache = {}
         self.streaming = streaming
+        self.log = get_logger("%s(%s)"%(self.__class__.__name__, self.id))
+        self.log.debug("__init__")
 
     def _mimetype(self, url):
         mimetype = mimetypes.guess_type(url)[0]
@@ -13,12 +19,15 @@ class BasicCache(object):
         return mimetype
 
     def __update(self, path):
+        self.log.debug("__update", path, self.streaming)
         if self.streaming:
             self.cache[path]['content'] = bool(os.stat(path).st_size)
+            self.log.debug("Content Present:", self.cache[path]['content'])
         else:
             f = open(path,'r')
             self.cache[path]['content'] = f.read()
             f.close()
+            self.log.debug("Content Length:", len(self.cache[path]['content']))
 
     def get_type(self, path):
         return self.cache[path]['type']
@@ -40,12 +49,15 @@ class BasicCache(object):
 
     def get(self, req, path, write_back, stream_back, err_back):
         if self._is_current(path):
+            self.log.debug("get", path, "CURRENT!")
             self._return(req, path, write_back, stream_back, err_back)
         elif os.path.isfile(path):
+            self.log.debug("get", path, "CREATING FROM FILE!")
             self._new_path(path, req.url)
             self.__update(path)
             self._return(req, path, write_back, stream_back, err_back)
         else:
+            self.log.debug("get", path, "404!")
             err_back(req)
 
 class NaiveCache(BasicCache):
