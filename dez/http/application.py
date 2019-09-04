7758fcfba7220b0479e9f2fa1d69152a50e6aa44
bubbleboy14 <mario.balibrera@gmail.com>
2019-09-03 19:09:07 -0700

    HTTPApplication: constructor takes static_dir_404 kwarg (default False), and passes value to StaticHandler

--- dez/http/application.py
+++ dez/http/application.py
@@ -18,13 +18,13 @@ class HTTPApplication(object):
         construct the appropriate RawHTTPResponse or HTTPResponse as required.
     """
 
-    def __init__(self, bind_address, port, get_logger=None, server_name="Dez", certfile=None, keyfile=None, cacerts=None, static_timestamp=False, rollz={}):
+    def __init__(self, bind_address, port, get_logger=None, server_name="Dez", certfile=None, keyfile=None, cacerts=None, static_timestamp=False, rollz={}, static_dir_404=False):
         """start listening on the given port (this doesn't include a call to
            event.dispatch)"""
         self.daemon = HTTPDaemon(bind_address, port, get_logger, certfile, keyfile, cacerts, rollz)
         self.host = bind_address
         self.port = port
-        self.static_request = StaticHandler(server_name, get_logger, static_timestamp)
+        self.static_request = StaticHandler(server_name, get_logger, static_timestamp, static_dir_404)
         self.wsgi_pool = None
         
     def start(self):
