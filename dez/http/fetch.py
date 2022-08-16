674d2b11a0183aadd61956c219b645358d75739f
bubbleboy14 <mario.balibrera@gmail.com>
2022-08-16 11:17:29 -0700

    HTTPClient: jayornay(); proc_resp(); multipart(); fetch(); post(). http.post()

--- dez/http/fetch.py
+++ dez/http/fetch.py
@@ -1,27 +1,22 @@
-from dez.json import decode
 from dez.http.client import HTTPClient
 
-F = None
+HC = None
 
-class Fetcher(HTTPClient):
-	def jayornay(self, txt, json=False):
-		if json:
-			return decode(txt)
-		return txt
+def http_client():
+	global HC
+	if not HC:
+		HC = HTTPClient()
+	return HC
 
-	def fetch(self, host, path="/", port=80, secure=False, headers={}, cb=None, timeout=1, json=False):
-		url = "%s://%s:%s%s"%(secure and "https" or "http", host, port, path)
-		self.log("fetching: %s"%(url,))
-		self.get_url(url, headers=headers,
-			cb=lambda resp : (cb or self.log)(self.jayornay(resp.body.get_value(), json)),
-			timeout=timeout)
+def do_dispatch():
+	import event
+	event.signal(2, event.abort)
+	event.dispatch()
 
 def fetch(host, path="/", port=80, secure=False, headers={}, cb=None, timeout=1, json=False, dispatch=False):
-	global F
-	if not F:
-		F = Fetcher()
-	F.fetch(host, path, port, secure, headers, cb, timeout, json)
-	if dispatch:
-		import event
-		event.signal(2, event.abort)
-		event.dispatch()
+	http_client().fetch(host, path, port, secure, headers, cb, timeout, json)
+	dispatch and do_dispatch()
+
+def post(host, path="/", port=80, secure=False, headers={}, data=None, text=None, cb=None, timeout=1, json=False, dispatch=False):
+	http_client().post(host, path, port, secure, headers, data, text, cb, timeout, json)
+	dispatch and do_dispatch()
