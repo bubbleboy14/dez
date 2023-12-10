575111dc6ce7354f49de517c84d9b72f4c0c9761
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-10 02:18:59 -0800

    http.fetch: http_client(), fetch(), and post() all now support silent (default True) kwarg

--- dez/http/fetch.py
+++ dez/http/fetch.py
@@ -2,10 +2,10 @@ from dez.http.client import HTTPClient
 
 HC = None
 
-def http_client():
+def http_client(silent=True):
 	global HC
 	if not HC:
-		HC = HTTPClient()
+		HC = HTTPClient(silent)
 	return HC
 
 def do_dispatch():
@@ -13,10 +13,10 @@ def do_dispatch():
 	event.signal(2, event.abort)
 	event.dispatch()
 
-def fetch(host, path="/", port=80, secure=False, headers={}, cb=None, timeout=1, json=False, dispatch=False):
-	http_client().fetch(host, path, port, secure, headers, cb, timeout, json)
+def fetch(host, path="/", port=80, secure=False, headers={}, cb=None, timeout=1, json=False, dispatch=False, silent=True):
+	http_client(silent).fetch(host, path, port, secure, headers, cb, timeout, json)
 	dispatch and do_dispatch()
 
-def post(host, path="/", port=80, secure=False, headers={}, data=None, text=None, cb=None, timeout=1, json=False, dispatch=False):
-	http_client().post(host, path, port, secure, headers, data, text, cb, timeout, json)
+def post(host, path="/", port=80, secure=False, headers={}, data=None, text=None, cb=None, timeout=1, json=False, dispatch=False, silent=True):
+	http_client(silent).post(host, path, port, secure, headers, data, text, cb, timeout, json)
 	dispatch and do_dispatch()
