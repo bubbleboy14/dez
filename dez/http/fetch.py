e08123a1dd188f8aace2a08673303dce54b822a3
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-22 16:30:22 -0800

    http.fetch, post()/fetch(): increased default timeout 5->10

--- dez/http/fetch.py
+++ dez/http/fetch.py
@@ -13,10 +13,10 @@ def do_dispatch():
 	event.signal(2, event.abort)
 	event.dispatch()
 
-def fetch(host, path="/", port=80, secure=False, headers={}, cb=None, timeout=5, json=False, dispatch=False, silent=SILENT, eb=None):
+def fetch(host, path="/", port=80, secure=False, headers={}, cb=None, timeout=10, json=False, dispatch=False, silent=SILENT, eb=None):
 	http_client(silent).fetch(host, path, port, secure, headers, cb, timeout, json, eb)
 	dispatch and do_dispatch()
 
-def post(host, path="/", port=80, secure=False, headers={}, data=None, text=None, cb=None, timeout=5, json=False, dispatch=False, silent=SILENT, eb=None):
+def post(host, path="/", port=80, secure=False, headers={}, data=None, text=None, cb=None, timeout=10, json=False, dispatch=False, silent=SILENT, eb=None):
 	http_client(silent).post(host, path, port, secure, headers, data, text, cb, timeout, json, eb=eb)
 	dispatch and do_dispatch()
