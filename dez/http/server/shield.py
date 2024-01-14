a4f11e87b0ba2c95cda7952b421819b670979c09
bubbleboy14 <mario.balibrera@gmail.com>
2024-01-14 15:55:32 -0800

    shield: replaced cf_scripts w/ more inclusive .cfc

--- dez/http/server/shield.py
+++ dez/http/server/shield.py
@@ -2,7 +2,7 @@ import event
 from dez.logging import default_get_logger
 
 BANNED_PRE = ["/", "~"]
-SKETCH_BITS = ["..", "/.", "/aws", "php", "boaform", "cf_scripts"]
+SKETCH_BITS = ["..", "/.", "/aws", "php", "boaform", ".cfc"]
 
 LIMIT = 200
 INTERVAL = 2
