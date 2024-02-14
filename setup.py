dc77bd65308a243d0c03748a96d2bc571d08e59c
bubbleboy14 <mario.balibrera@gmail.com>
2024-02-14 14:45:50 -0800

    vpush 0.10.10.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dez',
-    version='0.10.10.10',
+    version='0.10.10.11',
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
