a836e2e061918080b2cc4d55101e72373e1e7926
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-20 18:06:40 -0700

    shield: added .action to SKETCH_BITS[]

--- dez/http/server/shield.py
+++ dez/http/server/shield.py
@@ -2,7 +2,7 @@ import event
 from dez.logging import default_get_logger
 
 BANNED_PRE = ["/", "~"]
-SKETCH_BITS = ["..", "/.", ".sh", ".vm", ".cfc", ".dll", ".aspx",
+SKETCH_BITS = ["..", "/.", ".sh", ".vm", ".cfc", ".dll", ".aspx", ".action",
 	"/aws", "/sdk", "/ajax", "/resolve", "/query", "/dns-query", "/global-protect",
 	"php", "boaform", "goform", "cgi", "GponForm",
 	"/luci/", "/dana-na/", "/laravel/", "/redmine/",
