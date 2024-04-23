422b8097b56fbb6101295639a97b2c7d1bd610d7
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-23 01:42:23 -0700

    depv update: rel 0.4.9.10

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     zip_safe = False,
     install_requires = [
-        "rel >= 0.4.9.8",
+        "rel >= 0.4.9.10",
         "psutil >= 5.9.1",
         "python-magic >= 0.4.11"
     ],
