2f3992dd3de8fa59f8f5a0fc0f879cc6b02e8b77
bubbleboy14 <mario.balibrera@gmail.com>
2025-03-22 17:15:58 -0700

    shield: added "mstshash=", "/moodle/", and "gsocket.io" to SKETCH_BITS[]

--- dez/http/server/shield.py
+++ dez/http/server/shield.py
@@ -8,8 +8,8 @@ SKETCH_BITS = ["..", "/.", ".sh", ".vm", ".cfc", ".dll", ".aspx", ".alfa", ".act
 	"/dana-na/", "/laravel/", "/redmine/", "/webtools/", "/agent/", "/ALFA_DATA/", "/geoserver/",
 	"/v1/", "/cf_scripts/", "/onvif/", "/seeyon/", "/stalker_portal/", "/remote/", "/service/",
 	"/owa/", "/totp/", "/vpnsvc/", "/pfblockerng/", "/ztp/", "/owncloud/", "/luci/", "/filemanager/",
-	"/cms/", "/docker/", "/RDWeb/", "/kcfinder/", "/kubepi/", "/dup-installer/", "/pmd/",
-	"/private/", "/system/", "/zb_system/", "/cp/", "/_profiler/", "/__tests__/",
+	"/cms/", "/docker/", "/RDWeb/", "/kcfinder/", "/kubepi/", "/dup-installer/", "/pmd/", "/moodle/",
+	"/private/", "/system/", "/zb_system/", "/cp/", "/_profiler/", "/__tests__/", "gsocket.io", "mstshash=",
 	"/logincheck", "/MyCRL", "/Telerik", "/xmlrpc", "/wp-", "/FD87", ".html/", "/categories/Yud",
 	"androxgh0st", "3.1.05160", "4.10.05111", "system.listMethods", "0x01%5B%5D=legion", "0x%5B%5D=ridho",
 	"a=a", "debug=true", "debug=command", "username=admin&password=admin", "EX=_tools", "no-inspection-host=1"]
